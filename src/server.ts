66e9aa90b5035657a7ba6b8e47c32aed8f869c40
Michal Gawik <gawlik.zator@o2.pl>
2025-08-27 06:35:36 +0200

    install morgan types

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
 
+app.use(morgan('dev'))
+app.use(express.json())
+
 app.get('/', (req, res) => {
 	res.send('hello world')
+})
+
+app.post('/', (req, res) => {
+	res.json({ message: 'data received', data: req.body })
 })
 
 app.listen(5000, () => {
