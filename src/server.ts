bc83942fe5acf8fe013b2fb176b13eabe0cba455
Michal Gawik <gawlik.zator@o2.pl>
2025-08-28 06:42:18 +0200

    create jobs route

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,12 @@ dotenv.config()
 import express from 'express'
 const app = express()
 import morgan from 'morgan'
+import { nanoid } from 'nanoid'
+
+let jobs = [
+	{ id: nanoid(), company: 'google', position: 'front' },
+	{ id: nanoid(), company: 'microsoft', position: 'backend' },
+]
 
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
@@ -16,6 +22,10 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
 	res.json({ message: 'data received', data: req.body })
+})
+
+app.get('/api/v1/jobs', (req, res) => {
+	res.status(200).json({ jobs })
 })
 
 const port = process.env.PORT || 5000
