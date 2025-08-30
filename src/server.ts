529cc106a8eaa6db25e44bfbf9af81ac204b650d
Michal Gawik <gawlik.zator@o2.pl>
2025-08-30 07:05:15 +0200

    create logic to delete job

--- src/server.ts
+++ src/server.ts
@@ -76,6 +76,22 @@ app.patch('/api/v1/jobs/:id', (req: Request, res: Response) => {
 	res.status(200).json({ msg: 'job modified', job })
 })
 
+// DELETE JOB
+app.delete('/api/v1/jobs/:id', (req: Request, res: Response) => {
+	const { id } = req.params
+
+	const job = jobs.find(job => (job.id = id))
+	if (!job) {
+		return res.status(404).json({ msg: 'no job find' })
+	}
+
+	const newJobs = jobs.filter(job => job.id !== id)
+
+	jobs = newJobs
+
+	res.status(200).json({ msg: 'job deleted' })
+})
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
