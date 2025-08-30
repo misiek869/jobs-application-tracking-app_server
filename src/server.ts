c33333c667a68019120888f114f47cf4c34cbc4b
Michal Gawik <gawlik.zator@o2.pl>
2025-08-30 06:58:28 +0200

    create logic to edit job

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ app.get('/api/v1/jobs', (req: Request, res: Response) => {
 	res.status(200).json({ jobs })
 })
 
-// GET SINGLE JOBS
+// GET SINGLE JOB
 app.get('/api/v1/jobs/:id', (req: Request, res: Response) => {
 	const { id } = req.params
 
@@ -53,6 +53,27 @@ app.post('/api/v1/jobs', (req: Request, res: Response) => {
 	jobs.push(job)
 
 	res.status(201).json({ job })
+})
+
+// EDIT JOB
+app.patch('/api/v1/jobs/:id', (req: Request, res: Response) => {
+	const { company, position } = req.body
+
+	if (!company || !position) {
+		return res.status(400).json({ msg: 'please provide company and position' })
+	}
+
+	const { id } = req.params
+
+	const job = jobs.find(job => (job.id = id))
+	if (!job) {
+		return res.status(404).json({ msg: 'no job find' })
+	}
+
+	job.company = company
+	job.position = position
+
+	res.status(200).json({ msg: 'job modified', job })
 })
 
 const port = process.env.PORT || 5000
