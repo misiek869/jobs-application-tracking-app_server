44e4571ce7866d9ecdbc61ea8d66bb795df81d15
Michal Gawik <gawlik.zator@o2.pl>
2025-09-05 06:04:59 +0200

    create more custom errors

--- src/errors/customError.ts
+++ src/errors/customError.ts
@@ -2,10 +2,34 @@ import { StatusCodes } from 'http-status-codes'
 
 export class NotFoundError extends Error {
 	statusCode: number
-
 	constructor(message: string) {
 		super(message)
 		this.name = 'NotFoundError'
 		this.statusCode = StatusCodes.NOT_FOUND
+	}
+}
+
+export class BarRequestError extends Error {
+	statusCode: number
+	constructor(message: string) {
+		super(message)
+		this.name = 'BarRequestError'
+		this.statusCode = StatusCodes.BAD_REQUEST
+	}
+}
+export class UnauthenticatedError extends Error {
+	statusCode: number
+	constructor(message: string) {
+		super(message)
+		this.name = 'UnauthenticatedError'
+		this.statusCode = StatusCodes.UNAUTHORIZED
+	}
+}
+export class UnauthorizedError extends Error {
+	statusCode: number
+	constructor(message: string) {
+		super(message)
+		this.name = 'UnauthorizedError'
+		this.statusCode = StatusCodes.FORBIDDEN
 	}
 }
