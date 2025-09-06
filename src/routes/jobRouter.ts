42511535c94e5cd3dee2764986616bd1968a7184
Michal Gawik <gawlik.zator@o2.pl>
2025-09-06 06:37:29 +0200

    add validateJobInput to create and update route

--- src/routes/jobRouter.ts
+++ src/routes/jobRouter.ts
@@ -9,11 +9,12 @@ import {
 	editJob,
 	deleteJob,
 } from '../controllers/jobController.js'
+import { validateJobInput } from '../middleware/validationMIddleware.js'
 
 router.get('/', getAllJobs)
-router.post('/', createJob)
+router.post('/', validateJobInput, createJob)
 router.get('/:id', getSingleJob)
-router.patch('/:id', editJob)
+router.patch('/:id', validateJobInput, editJob)
 router.delete('/:id', deleteJob)
 
 export default router
