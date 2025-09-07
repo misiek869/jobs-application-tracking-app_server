2c8f4f1ce7c2172f61dffc97299e731d1e9134d4
Michal Gawik <gawlik.zator@o2.pl>
2025-09-07 06:40:51 +0200

    use validateRegister

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -3,8 +3,9 @@ import { Router } from 'express'
 const router = Router()
 
 import { register, login } from '../controllers/authController.js'
+import { validateRegister } from '../middleware/validationMIddleware.js'
 
-router.post('/register', register)
+router.post('/register', validateRegister, register)
 router.post('/login', login)
 
 export default router
