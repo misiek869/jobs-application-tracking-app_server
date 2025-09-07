a7fd4190d73c55c03fcb6c1554a61a7f270834d1
Michal Gawik <gawlik.zator@o2.pl>
2025-09-07 06:46:03 +0200

    create functionality to set first user as admin

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,6 +3,8 @@ import User from '../models/UserModel.js'
 import { StatusCodes } from 'http-status-codes'
 
 export const register = async (req: Request, res: Response) => {
+	const isFirstUser = (await User.countDocuments()) === 0
+	req.body.role = isFirstUser ? 'admin' : 'user'
 	const user = await User.create(req.body)
 	res.status(StatusCodes.CREATED).json({ user })
 }
