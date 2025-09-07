44024fd21dd611ec15c4d08c1e0ba1565c47ac03
Michal Gawik <gawlik.zator@o2.pl>
2025-09-07 07:48:47 +0200

    check email and password

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 import User from '../models/UserModel.js'
 import { StatusCodes } from 'http-status-codes'
 import { hashPassword } from '../utils/passwordUtils.js'
+import { UnauthenticatedError } from '../errors/customError.js'
 
 export const register = async (req: Request, res: Response) => {
 	const isFirstUser = (await User.countDocuments()) === 0
@@ -13,5 +14,9 @@ export const register = async (req: Request, res: Response) => {
 	res.status(StatusCodes.CREATED).json({ msg: 'user created' })
 }
 export const login = async (req: Request, res: Response) => {
+	const user = await User.findOne({ email: req.body.email })
+
+	if (!user) throw new UnauthenticatedError('invalid credentials')
+
 	res.send('login')
 }
