a587ec6e452c034b19f1e464c60a7fcb0c8d82ef
Michal Gawik <gawlik.zator@o2.pl>
2025-09-07 07:39:47 +0200

    validateLogin

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -3,9 +3,12 @@ import { Router } from 'express'
 const router = Router()
 
 import { register, login } from '../controllers/authController.js'
-import { validateRegister } from '../middleware/validationMIddleware.js'
+import {
+	validateRegister,
+	validateLogin,
+} from '../middleware/validationMIddleware.js'
 
 router.post('/register', validateRegister, register)
-router.post('/login', login)
+router.post('/login', validateLogin, login)
 
 export default router
