13fd8f93ae0f9f99f82868fa2333a8f83ea91706
Michal Gawik <gawlik.zator@o2.pl>
2025-09-07 07:33:39 +0200

    move hash password functionality to utils

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,15 +1,13 @@
 import { Request, Response, NextFunction } from 'express'
 import User from '../models/UserModel.js'
 import { StatusCodes } from 'http-status-codes'
-import bcrypt from 'bcrypt'
+import { hashPassword } from '../utils/passwordUtils.js'
 
 export const register = async (req: Request, res: Response) => {
 	const isFirstUser = (await User.countDocuments()) === 0
 	req.body.role = isFirstUser ? 'admin' : 'user'
 
-	const salt = await bcrypt.genSalt(10)
-	const hashedPassword = await bcrypt.hash(req.body.password, salt)
-	req.body.password = hashedPassword
+	req.body.password = await hashPassword(req.body.password)
 
 	const user = await User.create(req.body)
 	res.status(StatusCodes.CREATED).json({ msg: 'user created' })
