49f89231d337dd4f8af40660aa38e3f64fc4906c
Michal Gawik <gawlik.zator@o2.pl>
2025-09-08 05:55:48 +0200

    change values to env varibles

--- src/utils/tokenUtils.ts
+++ src/utils/tokenUtils.ts
@@ -2,8 +2,9 @@ import jwt, { Secret, SignOptions } from 'jsonwebtoken'
 
 export const createJWT = (
 	payload: object,
-	secret: Secret = process.env.JWT_SECRET || 'secret',
-	options: SignOptions = { expiresIn: '1d' }
+	secret: Secret = process.env.JWT_SECRET as Secret,
+	expiresIn: SignOptions['expiresIn'] = (process.env
+		.JWT_EXPIRES_IN as SignOptions['expiresIn']) || '1d'
 ): string => {
-	return jwt.sign(payload, secret, options)
+	return jwt.sign(payload, secret, { expiresIn })
 }
