0c1cdda0f66acf2de93a126e6c1e7caea7ee4c1b
Michal Gawik <gawlik.zator@o2.pl>
2025-09-16 05:46:24 +0200

    add validation to update user

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -4,11 +4,12 @@ import {
 	getCurrentUser,
 	updateUser,
 } from '../controllers/userController.js'
+import { validateUpdateUser } from '../middleware/validationMIddleware.js'
 
 const router = Router()
 
 router.get('/current-user', getCurrentUser)
 router.get('/admin/stats', getApplicationStats)
-router.patch('/update-user', updateUser)
+router.patch('/update-user', validateUpdateUser, updateUser)
 
 export default router
