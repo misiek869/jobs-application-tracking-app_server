13d2ae3da5825083ead9fc79dc2f7ba36a829522
Michal Gawik <gawlik.zator@o2.pl>
2025-09-20 06:07:11 +0200

    create componen

--- client/src/components/JobsContainer.tsx
+++ client/src/components/JobsContainer.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { UseAllJobsContext } from '../pages/AllJobs'
+import Job from './Job'
+import Wrapper from '../assets/wrappers/JobsContainer'
 
 const JobsContainer = () => {
-	return <div></div>
+	const { data } = UseAllJobsContext()
+	const { jobs } = data
+
+	if (jobs.length === 0) {
+		return (
+			<Wrapper>
+				<h2>The are no jobs...</h2>
+			</Wrapper>
+		)
+	}
+
+	return (
+		<Wrapper>
+			<div className='jobs'></div>
+			{jobs.map(job => {
+				return <Job key={join._id} {...job} />
+			})}
+		</Wrapper>
+	)
 }
 
 export default JobsContainer
