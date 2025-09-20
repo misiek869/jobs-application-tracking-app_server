463995d437a81a015a9d82b8383e0734c08c2e58
Michal Gawik <gawlik.zator@o2.pl>
2025-09-20 05:58:59 +0200

    create context

--- client/src/pages/AllJobs.tsx
+++ client/src/pages/AllJobs.tsx
@@ -1,16 +1,53 @@
 import { toast } from 'react-toastify'
-// import { JobsContainer, SearchContainer } from '../components'
+import { JobsContainer, SearchContainer } from '../components'
 import customFetch from '../utils/customFetch'
 import { useLoaderData } from 'react-router-dom'
 import { useContext, createContext } from 'react'
+// import { error } from 'console'
+import { AxiosError } from 'axios'
+
+type JobType = {
+	company: string
+	position: string
+	jobStatus: 'interview' | 'declined' | 'pending'
+	jobType: 'full-time' | 'part-time' | 'internship'
+	jobLocation: string
+	createdAt: string
+	updatedAt: string
+	createdBy: string
+}
+type JobsResponse = {
+	jobs: JobType[]
+	// totalJobs: number
+}
+
+type AllJobsContextType = JobsResponse
 
 export const loader = async () => {
-	const { data } = await customFetch.get('/jobs')
-	console.log(data)
+	try {
+		const { data } = await customFetch.get<JobsResponse>('/jobs')
+		return { data }
+	} catch (err) {
+		const error = err as AxiosError<{ message: string }>
+		const message = error.response?.data?.message || 'Something went wrong'
+		toast.error(message)
+		throw error
+	}
 }
 
+const AllJobsContext = createContext<AllJobsContextType | null>(null)
+
 const AllJobs = () => {
-	return <div>AllJobs</div>
+	const { data } = useLoaderData() as JobsResponse
+
+	return (
+		<AllJobsContext.Provider value={{ data }}>
+			<SearchContainer />
+			<JobsContainer />
+		</AllJobsContext.Provider>
+	)
 }
+
+export const UseAllJobsContext = () => useContext(AllJobsContext)
 
 export default AllJobs
