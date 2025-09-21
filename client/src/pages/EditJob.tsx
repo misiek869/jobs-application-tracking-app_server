d8c81d40120c1ab0feb5f367b914b3d6efb4cda1
Michal Gawik <gawlik.zator@o2.pl>
2025-09-21 07:31:41 +0200

    finisg edit functionality

--- client/src/pages/EditJob.tsx
+++ client/src/pages/EditJob.tsx
@@ -4,6 +4,7 @@ import {
 	useLoaderData,
 	useNavigation,
 	type LoaderFunctionArgs,
+	type ActionFunctionArgs,
 } from 'react-router-dom'
 import customFetch from '../utils/customFetch'
 import { AxiosError } from 'axios'
@@ -25,8 +26,20 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 	}
 }
 
-export const action = async () => {
-	return null
+export const action = async ({ request, params }: ActionFunctionArgs) => {
+	const formData = await request.formData()
+	const data = Object.fromEntries(formData)
+
+	try {
+		await customFetch.patch(`/jobs/${params.id}`, data)
+		toast.success('job edited')
+		return redirect('/dashboard/all-jobs')
+	} catch (err) {
+		const error = err as AxiosError<{ message: string }>
+		const message = error.response?.data?.message || 'Something went wrong'
+		toast.error(message)
+		return redirect('/dashboard/all-jobs')
+	}
 }
 
 type JobResponse = { job: JobType }
