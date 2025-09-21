26d09d8ae054ae63f81135e8048820aa1956f867
Michal Gawik <gawlik.zator@o2.pl>
2025-09-21 06:03:51 +0200

    styyle component

--- client/src/assets/wrappers/JobInfo.tsx
+++ client/src/assets/wrappers/JobInfo.tsx
@@ -1,5 +1,23 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.main``
+const Wrapper = styled.div`
+	display: flex;
+	align-items: center;
+
+	.job-icon {
+		font-size: 1rem;
+		margin-right: 1rem;
+		display: flex;
+		align-items: center;
+		svg {
+			color: var(--text-secondary-color);
+		}
+	}
+	.job-text {
+		text-transform: capitalize;
+		letter-spacing: var(--letter-spacing);
+		display: none;
+	}
+`
 
 export default Wrapper
