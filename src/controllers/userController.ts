4078ca94122938c61d0cc69eb29555a1a163cc0c
Michal Gawik <gawlik.zator@o2.pl>
2025-09-23 05:38:16 +0200

    update updateUser funct

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express'
 import User from '../models/UserModel.js'
 import Job from '../models/JobModel.js'
 import { StatusCodes } from 'http-status-codes'
+import { v2 as cloudinary } from 'cloudinary'
+import { promises as fs } from 'fs'
 
 export const getCurrentUser = async (req: Request, res: Response) => {
 	const user = await User.findOne({ _id: req.user?.userId }).select('-password')
@@ -17,12 +19,24 @@ export const getApplicationStats = async (req: Request, res: Response) => {
 }
 
 export const updateUser = async (req: Request, res: Response) => {
-	console.log(req.file)
+	const newUser = { ...req.body }
 
-	const updatedUser = await User.findByIdAndUpdate(
-		req.user?.userId,
-		req.body
-	).select('-password')
+	if (req.file) {
+		const response = await cloudinary.uploader.upload(req.file.path)
+
+		await fs.unlink(req.file.path)
+
+		newUser.avatar = response.secure_url
+		newUser.avatarPublicId = response.public_id
+	}
+
+	const updatedUser = await User.findByIdAndUpdate(req.user?.userId, newUser, {
+		new: true,
+	}).select('-password')
+
+	if (req.file && updatedUser?.avatarPublicId) {
+		await cloudinary.uploader.destroy(updatedUser.avatarPublicId)
+	}
 
 	res.status(StatusCodes.OK).json({ msg: 'update user' })
 }
