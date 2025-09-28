2bed3d61238e5dfc71e4df288aa4e118bf48bf6a
Michal Gawik <gawlik.zator@o2.pl>
2025-09-28 06:35:54 +0200

    create stats page draft

--- client/src/components/ChartsContainer.tsx
+++ client/src/components/ChartsContainer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ChartsContainer = () => {
-	return <div>ChartsContainer</div>
+	return <div></div>
 }
 
 export default ChartsContainer
--- client/src/pages/Stats.tsx
+++ client/src/pages/Stats.tsx
@@ -1,9 +1,34 @@
+import customFetch from '../utils/customFetch'
+import { AxiosError } from 'axios'
+import { useLoaderData } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { ChartsContainer, StatsContainer } from '../components'
+import BarChart from '../components/BarChart'
+
 export const loader = async () => {
-	return null
+	try {
+		const response = await customFetch('/jobs/stats')
+		return response.data
+	} catch (err) {
+		const error = err as AxiosError<{ message: string }>
+		const message = error.response?.data?.message || 'Something went wrong'
+		toast.error(message)
+		throw error
+	}
 }
 
 const Stats = () => {
-	return <div>Stats</div>
+	const { defaultStats, monthlyApplications } = useLoaderData()
+
+	return (
+		<>
+			<StatsContainer defaultStats={defaultStats} />
+
+			{monthlyApplications?.length > 1 && (
+				<ChartsContainer data={monthlyApplications} />
+			)}
+		</>
+	)
 }
 
 export default Stats
