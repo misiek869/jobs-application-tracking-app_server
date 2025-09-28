d94e1f602abfdb749c4c7ef50ac3ebf17d6ede8a
Michal Gawik <gawlik.zator@o2.pl>
2025-09-28 07:54:13 +0200

    style component

--- client/src/assets/wrappers/ChartsContainer.ts
+++ client/src/assets/wrappers/ChartsContainer.ts
@@ -1,5 +1,25 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.main`c`
+const Wrapper = styled.section`
+	margin-top: 4rem;
+	text-align: center;
+
+	button {
+		background: var(--primary-500);
+		border-color: transparent;
+		text-transform: capitalize;
+		padding: 0.5rem 1rem;
+		border-radius: var(--border-radius);
+
+		color: white;
+		font-size: 1.25rem;
+		cursor: pointer;
+	}
+
+	h4 {
+		text-align: center;
+		margin-bottom: 1rem;
+	}
+`
 
 export default Wrapper
