bb9326a3bf0b7d7c2a2086610e17681a9175f285
Michal Gawik <gawlik.zator@o2.pl>
2025-09-28 07:19:42 +0200

    component complete

--- client/src/components/ChartsContainer.tsx
+++ client/src/components/ChartsContainer.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
+import Wrapper from '../assets/wrappers/ChartsContainer'
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
 
-const ChartsContainer = () => {
-	return <div></div>
+type ChartsContainerProps = {
+	date: string
+	count: number
+}
+
+const ChartsContainer = ({ data }: { data: ChartsContainerProps }) => {
+	const [barChart, setBarChart] = useState<boolean>(true)
+
+	return (
+		<Wrapper>
+			<h4>Monthly Applications</h4>
+			<button onClick={() => setBarChart(!barChart)}>
+				{barChart ? 'Area Chart' : 'Bar Chart'}
+			</button>
+			{barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+		</Wrapper>
+	)
 }
 
 export default ChartsContainer
