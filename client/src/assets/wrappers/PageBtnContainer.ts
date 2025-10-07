1e16ceb560c55df86100c7fa16779896d2083d61
Michal Gawik <gawlik.zator@o2.pl>
2025-10-07 06:17:43 +0200

    style page btns

--- client/src/assets/wrappers/PageBtnContainer.ts
+++ client/src/assets/wrappers/PageBtnContainer.ts
@@ -1,28 +1,65 @@
 import styled from 'styled-components'
 
-const Wrapper = styled.main`
-	min-height: 100vh;
-	text-align: center;
+const Wrapper = styled.section`
+	height: 6rem;
+	margin-top: 2rem;
 	display: flex;
 	align-items: center;
-	justify-content: center;
+	justify-content: end;
+	flex-wrap: wrap;
+	gap: 1rem;
 
-	img {
-		width: 90vw;
-		max-width: 600px;
-		display: block;
-		margin-top: -3rem;
-		margin-bottom: 2rem;
+	.btn-container {
+		background: var(--background-secondary-color);
+		border-radius: var(--border-radius);
+		display: flex;
 	}
 
-	h3 {
-		margin-bottom: 1.5rem;
+	.page-btn {
+		background: transparent;
+		border-color: transparent;
+		width: 50px;
+		height: 40px;
+		font-weight: 700;
+		font-size: 1.25rem;
+		color: var(--primary-500);
+		border-radius: var(--border-radius);
+		cursor: pointer;
 	}
 
-	a {
+	.active {
+		background: var(--primary-500);
+		color: var(--white);
+	}
+
+	.prev-btn,
+	.next-btn {
+		background: var(--background-secondary-color);
+		border-color: transparent;
+		border-radius: var(--border-radius);
+		width: 100px;
+		height: 40px;
 		color: var(--primary-500);
 		text-transform: capitalize;
-		font-size: 1.5rem;
+		letter-spacing: var(--letter-spacing);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 0.5rem;
+		cursor: pointer;
+	}
+
+	.prev-btn:hover,
+	.next-btn:hover {
+		background: var(--primary-500);
+		color: var(--white);
+		transition: var(--transition);
+	}
+
+	.dots {
+		display: grid;
+		place-content: center;
+		cursor: text;
 	}
 `
 
